4d406fcf1cefb6a1c120b39ccef7634f33d4b923
Gergely Imreh <imrehg@gmail.com>
2016-04-22 16:27:07 +0800

    add device loading code

--- seeeduino.js
+++ seeeduino.js
@@ -5,7 +5,16 @@
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
+        if (!connected) {
+            return { status:1, msg:'Disconnected' };
+        } else {
+            return { status:2, msg:'Connected' };
+        }
+    };
+
+    ext._deviceRemoved = function(dev) {
+        console.log('Device removed');
+        // Not currently implemented with serial devices
     };
 
     // Block and block menu descriptions
